dd026c4fcd8e6e8e95d209fa49055ce1bc52b226
Gustavo Barone <gbarone.1406@gmail.com>
2023-10-07 21:44:41 -0300

    delete python file

--- teste/teste.py
+++
@@ -1,9 +0,0 @@
-try:
-    a = int(input())
-    b = int(input())
-
-    x = a + b
-
-    print("X =", x)
-except ValueError:
-    print("Entrada inválida. Certifique-se de inserir números inteiros.")
